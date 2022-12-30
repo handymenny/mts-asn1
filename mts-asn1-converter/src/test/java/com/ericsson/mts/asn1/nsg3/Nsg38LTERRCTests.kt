f9a7ef30910d434d418f1a6fc02f0ceba88048fe
handymenny <handymenny@outlook.com>
2022-12-30 21:29:38 +0100

    mts-asn1-converter: Add NSG 3.6 and 3.8 tests

---
+++ mts-asn1-converter/src/test/java/com/ericsson/mts/asn1/nsg3/Nsg38LTERRCTests.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2022 Andrea Mennillo, git at amennillo dot eu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.ericsson.mts.asn1.nsg3
+
+import com.ericsson.mts.asn1.ASN1Converter
+import com.ericsson.mts.asn1.AbstractTests
+import com.ericsson.mts.asn1.converter.ConverterNSG
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+
+class Nsg38LTERRCTests : AbstractTests() {
+
+    @Test
+    @Throws(Exception::class)
+    fun testUEEutraCapability() {
+        test(
+            "UE-EUTRA-Capability",
+            "/data/NSG3/3.8_and_above/UEEutraCapability.txt",
+            "/data/oracle/UEEutraCapability.json",
+            "/data/oracle/UEEutraCapability.xml"
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUEEutraCapabilityKirin() {
+        test(
+            "UE-EUTRA-Capability",
+            "/data/NSG3/3.8_and_above/UEEutraCapabilityKirin.txt",
+            "/data/oracle/UEEutraCapabilityKirin.json",
+            "/data/oracle/UEEutraCapabilityKirin.xml"
+        )
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testUEEutraCapabilityRequestDiffFallback() {
+        test(
+            "UE-EUTRA-Capability",
+            "/data/NSG3/3.8_and_above/UEEutraCapabilityRequestDiffFallback.txt",
+            "/data/oracle/UEEutraCapabilityRequestDiffFallback.json",
+            "/data/oracle/UEEutraCapabilityRequestDiffFallback.xml"
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUEEutraCapabilitySkipFallback() {
+        test(
+            "UE-EUTRA-Capability",
+            "/data/NSG3/3.8_and_above/UEEutraCapabilitySkipFallback.txt",
+            "/data/oracle/UEEutraCapabilitySkipFallback.json",
+            "/data/oracle/UEEutraCapabilitySkipFallback.xml"
+        )
+    }
+
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            try {
+                asn1Converter = ASN1Converter(ConverterNSG(), listOf(
+                    getResourceAsStream(
+                        "/grammar/LTERRC/EUTRA-RRC-Definitions.asn")!!))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
---
+++ mts-asn1-converter/src/test/java/com/ericsson/mts/asn1/nsg3/Nsg38NRRRCTests.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2022 Andrea Mennillo, git at amennillo dot eu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.ericsson.mts.asn1.nsg3
+
+import com.ericsson.mts.asn1.ASN1Converter
+import com.ericsson.mts.asn1.AbstractTests
+import com.ericsson.mts.asn1.converter.ConverterNSG
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+class Nsg38NRRRCTests : AbstractTests() {
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testUeMrdcCapability() {
+        test(
+            "UE-MRDC-Capability",
+            "/data/NSG3/3.8_and_above/UEMrdcCapability.txt",
+            "/data/oracle/UeMrdcCapability.json",
+            "/data/oracle/UeMrdcCapability.xml"
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUeMrdcCapabilityKirin() {
+        test(
+            "UE-MRDC-Capability",
+            "/data/NSG3/3.8_and_above/UeMrdcCapabilityKirin.txt",
+            "/data/oracle/UeMrdcCapabilityKirin.json",
+            "/data/oracle/UeMrdcCapabilityKirin.xml"
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUeNrCapability() {
+        test(
+            "UE-NR-Capability",
+            "/data/NSG3/3.8_and_above/UeNrCapability.txt",
+            "/data/oracle/UeNrCapability.json",
+            "/data/oracle/UeNrCapability.xml"
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUeNrCapabilityKirin() {
+        test(
+            "UE-NR-Capability",
+            "/data/NSG3/3.8_and_above/UeNrCapabilityKirin.txt",
+            "/data/oracle/UeNrCapabilityKirin.json",
+            "/data/oracle/UeNrCapabilityKirin.xml"
+        )
+    }
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            try {
+                asn1Converter = ASN1Converter(ConverterNSG(), listOf(
+                    getResourceAsStream(
+                        "/grammar/NRRRC/NR-RRC-Definitions.asn")!!))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
