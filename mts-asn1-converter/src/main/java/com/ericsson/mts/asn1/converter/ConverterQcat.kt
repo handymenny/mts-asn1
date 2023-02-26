35c3b0821794bc37ca71f14072b9d93b61c6c940
handymenny <handymenny@outlook.com>
2023-02-26 21:22:12 +0100

    mts-converter: Add Qcat converter

---
+++ mts-asn1-converter/src/main/java/com/ericsson/mts/asn1/converter/ConverterQcat.kt
@@ -0,0 +1,363 @@
+package com.ericsson.mts.asn1.converter
+
+import com.ericsson.mts.asn1.ASN1Parser.*
+import java.util.*
+
+/**
+ * Implementation of [AbstractConverter] for qcat Logs
+ */
+class ConverterQcat : AbstractConverter() {
+    private val bitsRegex by lazy { """\s'([01\s]+)'B""".toRegex() }
+
+    // false -> array, true -> Object
+    private val isObjectStack: Stack<Boolean> = Stack()
+
+    override fun cleanup(lines: List<String>): List<String> {
+        return lines
+    }
+
+    override fun preprocessLine(line: String, identifier: String, indentationLevel: Int): Boolean {
+        // Skip blank lines
+        if (indentationLevel == line.length) {
+            return true
+        }
+
+        // pop stacks when line contains "}"
+        if (line.substring(indentationLevel).startsWith("}")) {
+            if (isObjectStack.size > 0) {
+                val isObject = isObjectStack.pop()
+                if (isObject) {
+                    typesStack.pop()
+                    writer.leaveObject(null)
+                } else {
+                    writer.leaveArray(null)
+                }
+            }
+            return true
+        }
+
+        return false
+    }
+
+    override fun popStacks(indentation: Int) {
+        if (indentation == -1) {
+            while (isObjectStack.size > 0) {
+                val isObject = isObjectStack.pop()
+                if (isObject) {
+                    typesStack.pop()
+                    writer.leaveObject(null)
+                } else {
+                    writer.leaveArray(null)
+                }
+            }
+        }
+    }
+
+    override fun parseSequence(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: SequenceTypeContext
+    ): Int {
+        isObjectStack.push(true)
+        writer.enterObject(identifier)
+        typesStack.push(
+            combineComponentListAndAdditions(
+                context.componentTypeLists().rootComponentTypeList(0).componentTypeList()
+                    .componentType(),
+                context.componentTypeLists().extensionAdditions()?.extensionAdditionList()
+                    ?.extensionAddition() ?: emptyList()
+            )
+        )
+        return 2
+    }
+
+    override fun parseEnumerated(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: EnumeratedTypeContext
+    ): Int {
+        writer.stringValue(identifier, getValue(lineArray[index], indentation))
+        return 1
+    }
+
+    override fun parseBitString(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: BitStringTypeContext
+    ): Int {
+        val line = lineArray[index]
+        val minLength = getLowerBound(identifier)?.intValueExact() ?: 0
+        var bits = getBitsValue(line) ?: ""
+        if (bits.length < minLength) {
+            // Pad if needed
+            bits = bits.padEnd(minLength, '0')
+        }
+        writer.bitsValue(identifier, bits)
+        return 1
+    }
+
+    override fun parseInteger(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: IntegerTypeContext
+    ): Int {
+        writer.intValue(identifier, getValue(lineArray[index], indentation).toBigInteger(), null)
+        return 1
+    }
+
+    override fun parseObjectClassField(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: ObjectClassFieldTypeContext
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseObjectidentifier(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: ObjectidentifiertypeContext
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseBuiltin(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: BuiltinTypeContext
+    ): Int {
+        if (context.BOOLEAN_LITERAL() != null) {
+            writer.booleanValue(identifier, getValue(lineArray[index], indentation).toBoolean())
+        }
+        return 1
+    }
+
+    override fun parseCharacterString(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: CharacterStringTypeContext
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseSequenceOf(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: SequenceOfTypeContext
+    ): Int {
+        var read = 1
+        writer.enterArray(identifier)
+        isObjectStack.push(false)
+        val subtypeIdentifier = context.asnType().referencedType().definedType().IDENTIFIER()[0]
+        when (val subType = getType(subtypeIdentifier.toString())) {
+            is SequenceTypeContext -> {
+                read++
+                val subTypeComponents =
+                    combineComponentListAndAdditions(
+                        subType.componentTypeLists().rootComponentTypeList(0).componentTypeList()
+                            .componentType(),
+                        subType.componentTypeLists().extensionAdditions()?.extensionAdditionList()
+                            ?.extensionAddition() ?: emptyList()
+                    )
+                var nextLine: String = lineArray.getOrNull(index + read) ?: return read
+
+                val stackSize = isObjectStack.size
+                while (isObjectStack.size >= stackSize) {
+                    if (isObjectStack.size == stackSize && nextLine.contains("{")) {
+                        typesStack.push(subTypeComponents)
+                        isObjectStack.push(true)
+                        writer.enterObject(identifier)
+                        read++
+                    } else {
+                        read += processLines(index + read, lineArray)
+                    }
+                    nextLine = lineArray.getOrNull(index + read) ?: break
+                }
+            }
+
+            is EnumeratedTypeContext -> {
+                read++
+                var nextLine = lineArray.getOrNull(index + read) ?: return read
+                var newLevel = getIndentationLevel(nextLine)
+                while (!nextLine.contains("}")) {
+                    writer.stringValue(identifier, getValue(nextLine, newLevel))
+                    read++
+                    nextLine = lineArray.getOrNull(index + read) ?: return read
+                    newLevel = getIndentationLevel(nextLine)
+                }
+            }
+
+            is SequenceOfTypeContext -> {
+                val stackSize = isObjectStack.size
+                var nextLine: String
+                while (isObjectStack.size >= stackSize) {
+                    read += processLines(
+                        index + read, lineArray, overrideType = subType,
+                        overrideIdentifier = identifier
+                    )
+                    nextLine = lineArray.getOrNull(index + read) ?: return read
+                    if (isObjectStack.size == stackSize && nextLine.contains("}")) {
+                        writer.leaveArray(identifier)
+                        isObjectStack.pop()
+                        read++
+                    }
+                }
+            }
+
+            is IntegerTypeContext -> {
+                read++
+                var nextLine = lineArray.getOrNull(index + read) ?: return read
+                var newLevel = getIndentationLevel(nextLine)
+                while (!nextLine.contains("}")) {
+                    writer.intValue(identifier, getValue(nextLine, newLevel).toBigInteger(), null)
+                    read++
+                    nextLine = lineArray.getOrNull(index + read) ?: return read
+                    newLevel = getIndentationLevel(nextLine)
+                }
+            }
+
+            is ChoiceTypeContext -> {
+                val indentationObject = subType.alternativeTypeLists()
+                    .rootAlternativeTypeList()
+                    .alternativeTypeList()
+                    .namedType()
+                var nextLine = lineArray.getOrNull(index + read) ?: return read
+                val stackSize = isObjectStack.size
+                while (isObjectStack.size >= stackSize) {
+                    if (isObjectStack.size == stackSize && nextLine.contains(":")) {
+                        typesStack.push(indentationObject)
+                        isObjectStack.push(true)
+                        writer.enterObject(null)
+                    }
+                    read += processLines(index + read, lineArray)
+                    if (nextLine.contains("}") && isObjectStack.size == stackSize + 1) {
+                        typesStack.pop()
+                        isObjectStack.pop()
+                        writer.leaveObject(null)
+                    }
+                    nextLine = lineArray.getOrNull(index + read) ?: return read
+                }
+            }
+
+            else -> {
+                // Do nothing
+            }
+        }
+        return read
+    }
+
+    override fun parseSet(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: SetTypeContext
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseSetOf(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: SetOfTypeContext
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseOctetString(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: OctetStringTypeContext
+    ): Int {
+        var read = 1
+        val subType = getContaining(identifier)
+        if (subType != null) {
+            read = processLines(index, lineArray, overrideType = subType, overrideIdentifier = identifier)
+        }
+        return read
+    }
+
+    override fun parseReal(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: RealTypeContext
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseChoiceType(
+        index: Int,
+        lineArray: List<String>,
+        identifier: String,
+        indentation: Int,
+        context: ChoiceTypeContext
+    ): Int {
+        var read = 0
+        if (context.alternativeTypeLists().rootAlternativeTypeList().alternativeTypeList() != null) {
+            isObjectStack.push(true)
+            writer.enterObject(identifier)
+            typesStack.push(
+                context.alternativeTypeLists().rootAlternativeTypeList().alternativeTypeList()
+                    .namedType()
+            )
+        }
+        val preStackSize = isObjectStack.size
+        read += processLines(
+            index,
+            lineArray,
+            overrideIdentifier = lineArray[index].substring(indentation).split(" ").getOrNull(1)
+        )
+        val postStackSize = isObjectStack.size
+
+        if (postStackSize > preStackSize) {
+            while (isObjectStack.size >= postStackSize) {
+                read += processLines(index + read, lineArray)
+            }
+        }
+
+        if (postStackSize > preStackSize) {
+            typesStack.pop()
+            isObjectStack.pop()
+            writer.leaveObject(null)
+        } else {
+            isObjectStack.pop()
+            writer.leaveObject(null)
+            typesStack.pop()
+        }
+        return read
+    }
+
+
+    private fun getBitsValue(line: String): String? {
+        return bitsRegex.find(line)?.groupValues?.get(1)?.replace(" ", "")
+
+    }
+
+    private fun getValue(line: String, indentation: Int): String {
+        return line.substring(indentation).split(" ").last().trimEnd(',')
+    }
+}
---
+++ mts-asn1-converter/src/test/java/com/ericsson/mts/asn1/qcat/QcatNRRRCTests.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 Andrea Mennillo, git at amennillo dot eu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.ericsson.mts.asn1.qcat
+
+import com.ericsson.mts.asn1.ASN1Converter
+import com.ericsson.mts.asn1.AbstractTests
+import com.ericsson.mts.asn1.converter.ConverterQcat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+class QcatNRRRCTests : AbstractTests() {
+    @Test
+    @Throws(Exception::class)
+    fun testUeNrCapability3CC() {
+        test(
+            "UE-NR-Capability",
+            "/data/qcat/UeNrCapability3CC.txt",
+            "/data/oracle/UeNrCapability3CC.json",
+            "/data/oracle/UeNrCapability3CC.xml"
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUeMrdcCapabilityFR2() {
+        test(
+            "UE-MRDC-Capability",
+            "/data/qcat/UeMrdcCapabilityFR2.txt",
+            "/data/oracle/UeMrdcCapabilityFR2.json",
+            "/data/oracle/UeMrdcCapabilityFR2.xml"
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testUeMrdcCapabilityIpad() {
+        test(
+            "UE-MRDC-Capability",
+            "/data/qcat/UeMrdcCapabilityIpad.txt",
+            "/data/oracle/UeMrdcCapabilityIpad.json",
+            "/data/oracle/UeMrdcCapabilityIpad.xml"
+        )
+    }
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            try {
+                asn1Converter = ASN1Converter(
+                    ConverterQcat(), listOf(
+                        getResourceAsStream(
+                            "/grammar/NRRRC/NR-RRC-Definitions.asn"
+                        )!!
+                    )
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
